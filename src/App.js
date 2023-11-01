5de8f77143eccc7a0dc21c49a55f8a7bc2afe59a
Rebekah Callari-Kaczmarczyk <rebekahcallari@gmail.com>
2023-10-31 20:07:41 -0400

    lift state, destructure props, add constants

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./Button";
 
+const BUTTON_ONE = "buttonOne";
+const BUTTON_TWO = "buttonTwo";
+
 function App() {
+  const [totalClickCount, setTotalClickCount] = useState({
+    [BUTTON_ONE]: 0,
+    [BUTTON_TWO]: 0,
+  });
+
+  const handleClick = (key) => {
+    const newClickCount = totalClickCount[key] + 1;
+    const newClickCountObject = { ...totalClickCount };
+    newClickCountObject[key] = newClickCount;
+    setTotalClickCount(newClickCountObject);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <Button label="Click Me!" initialCount={0} />
+        <Button
+          id={BUTTON_ONE}
+          label="Button 1"
+          handleClick={handleClick}
+          totalClickCount={totalClickCount}
+        />
+        <br></br>
+        <Button
+          id={BUTTON_TWO}
+          label="Button 2"
+          handleClick={handleClick}
+          totalClickCount={totalClickCount}
+        />
+        <br></br>
+        <h2>{`You have clicked a total of ${
+          totalClickCount.buttonOne + totalClickCount.buttonTwo
+        } times`}</h2>
       </header>
     </div>
   );
--- src/Button.js
+++ src/Button.js
@@ -1,26 +1,15 @@
-import { useState } from "react";
-
-///// Requirements /////
-// There is one button displayed on the page
-// When the user clicks the button, the app will track the number of clicks of the user
-// The number of clicks will be displayed to the user
-
-const Button = (props) => {
-  const initialCount = props.initialCount ?? 0;
-  const [clickCount, setClickCount] = useState(initialCount);
-  console.log("click count", clickCount);
-
-  const handleClick = () => {
-    const newClickCount = clickCount + 1;
-    setClickCount(newClickCount);
+const Button = ({ id, label, handleClick, totalClickCount }) => {
+  const handleButtonClick = () => {
+    handleClick(id);
   };
+
   return (
     <>
-      <h4>{`Your initial count is ${initialCount}`}</h4>
-      <button onClick={handleClick}>{props.label}</button>
-      <h4>{`You have clicked ${clickCount} ${
-        clickCount === 1 ? "time" : "times"
-      }`}</h4>
+      <button onClick={handleButtonClick}>{label}</button>
+
+      <p>{`You have clicked the ${label} button ${
+        totalClickCount ? totalClickCount[id] : 0
+      } ${totalClickCount && totalClickCount[id] === 1 ? "time" : "times"}`}</p>
     </>
   );
 };
